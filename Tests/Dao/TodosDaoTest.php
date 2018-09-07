4ade28d06d0c1683f8430308a39fa75b75a1a2ac
tdorkaa <tamasdorkaa@gmail.com>
2018-09-07 10:28:32 +0200

    refactor test

--- Tests/Dao/TodosDaoTest.php
+++ Tests/Dao/TodosDaoTest.php
@@ -9,14 +9,29 @@ use ToDoApp\Entity\Todo;
 class TodosDaoTest extends TestCase
 {
     /**
+     * @var PDO
+     */
+    private $PDO;
+    protected function setUp()
+    {
+        $this->PDO = (new PdoFactory(new EnvironmentLoader()))->getPDO();
+        $this->PDO->query('TRUNCATE TABLE todos');    }
+
+    /**
      * @test
      */
     public function listTodos_GivenOneTodoInTheDb_ReturnsTodo()
     {
         $todo = new Todo('todo name', 'todo description', '2018-08-29 10:00:00');
-        $PDO = (new PdoFactory(new EnvironmentLoader()))->getPDO();
-        $PDO->query('TRUNCATE TABLE todos');
+        $this->createTodos([$todo]);
 
+        $todosDao = new TodosDao($this->PDO);
+        $result = $todosDao->listTodos();
+        $this->assertEquals($todo, $result[0]);
+    }
+
+    private function createTodos($todos)
+    {
         $sql = '
             INSERT INTO todos (
                 name, 
@@ -31,16 +46,14 @@ class TodosDaoTest extends TestCase
                 :due_at
             )
         ';
-        $statement = $PDO->prepare($sql);
-        $statement->execute(array(
-            ':name' => $todo->getName(),
-            ':description' => $todo->getDescription(),
-            ':status' => $todo->getStatus(),
-            ':due_at' => $todo->getDueAt()
-        ));
-
-        $todosDao = new TodosDao($PDO);
-        $result = $todosDao->listTodos();
-        $this->assertEquals($todo, $result[0]);
+        $statement = $this->PDO->prepare($sql);
+        foreach ($todos as $todo) {
+            $statement->execute(array(
+                ':name' => $todo->getName(),
+                ':description' => $todo->getDescription(),
+                ':status' => $todo->getStatus(),
+                ':due_at' => $todo->getDueAt()
+            ));
+        }
     }
 }
