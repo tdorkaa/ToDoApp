dd56d3156ef3f1f170eb02884755108c5b046a17
tdorkaa <tamasdorkaa@gmail.com>
2018-09-13 11:00:41 +0200

    sanitize description input

--- Tests/Sanitizer/InputSanitizerTest.php
+++ Tests/Sanitizer/InputSanitizerTest.php
@@ -38,4 +38,15 @@ class InputSanitizerTest extends TestCase
         $this->assertEquals($todoSanitized, $actual);
     }
 
+    /**
+     * @test
+     */
+    public function sanitize_GivenTodoDescriptionBeginsWithExtraSpaces_ReturnsTheTrimmedTodo()
+    {
+        $todo = new Todo(null, 'todo name', '            todo desc', '2018-08-29 10:00:00');
+        $todoSanitized = new Todo(null, 'todo name', 'todo desc', '2018-08-29 10:00:00');
+        $actual = $this->inputSanitizer->sanitize($todo);
+        $this->assertEquals($todoSanitized, $actual);
+    }
+
 }
--- src/Sanitizer/InputSanitizer.php
+++ src/Sanitizer/InputSanitizer.php
@@ -10,6 +10,7 @@ class InputSanitizer
     public function sanitize(Todo $todo)
     {
         $todo->setName(trim($todo->getName()));
+        $todo->setDescription(trim($todo->getDescription()));
         return $todo;
     }
 }
